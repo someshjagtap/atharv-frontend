74165c044828649c6d0eb6c5b59fb5111bbcf194
someshjagtap <someshjagtap12@gmail.com>
2023-05-10 19:36:33 +0530

    Added user form

--- src/app/routes/user/create/create.component.ts
+++ src/app/routes/user/create/create.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent {
+  securityUserDTO: any = {};
+  branchList: any = [];
+  organizationList: any = [];
+  securityRoleList: any = [];
+  selectedBranchId: number = 0;
+  selectorgId: number = 0;
+  selectedRoleId: number = 0;
 
+  save(){
+
+  }
+
+  previousState(){
+
+  }
 }
